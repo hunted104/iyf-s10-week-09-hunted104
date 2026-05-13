ca673cfc49b34caa721dbff41f8e014a9d30d190
hunted104 <mutugian25@gmail.com>
2026-05-13 12:16:54 -0700

    Update PostDetail.jsx

--- pages/PostDetail.jsx
+++ pages/PostDetail.jsx
@@ -1,1 +1,22 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import ErrorMessage from "../components/common/ErrorMessage";
 
+export default function PostDetail() {
+  const { postId } = useParams();
+
+  const { data, loading, error } = useFetch(
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
+  );
+
+  if (loading) return <LoadingSpinner />;
+  if (error) return <ErrorMessage message={error} />;
+
+  return (
+    <div>
+      <h2>{data.title}</h2>
+      <p>{data.body}</p>
+    </div>
+  );
+}
