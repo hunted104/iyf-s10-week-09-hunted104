4fab68286653404ca286eed1f5f7794df3186caf
hunted104 <mutugian25@gmail.com>
2026-05-13 12:16:23 -0700

    Update Posts.jsx

--- pages/Posts.jsx
+++ pages/Posts.jsx
@@ -1,1 +1,21 @@
+import useFetch from "../hooks/useFetch";
+import PostCard from "../components/posts/PostCard";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import ErrorMessage from "../components/common/ErrorMessage";
 
+export default function Posts() {
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  if (loading) return <LoadingSpinner />;
+  if (error) return <ErrorMessage message={error} />;
+
+  return (
+    <div>
+      {data.slice(0, 10).map(post => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
+}
