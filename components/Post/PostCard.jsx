4b75c8e335dfb900bb504d7150aef0536c7b9c68
hunted104 <mutugian25@gmail.com>
2026-05-13 12:15:38 -0700

    Update PostCard.jsx

--- components/Post/PostCard.jsx
+++ components/Post/PostCard.jsx
@@ -1,1 +1,11 @@
+import { Link } from "react-router-dom";
 
+export default function PostCard({ post }) {
+  return (
+    <div style={{ border: "1px solid #ccc", margin: "10px", padding: "10px" }}>
+      <h3>{post.title}</h3>
+      <p>{post.body.slice(0, 80)}...</p>
+      <Link to={`/posts/${post.id}`}>Read More</Link>
+    </div>
+  );
+}
