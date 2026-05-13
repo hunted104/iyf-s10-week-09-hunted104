e56b6e06ef2ff4e4d96b14eefa76f2c88b360a6d
hunted104 <mutugian25@gmail.com>
2026-05-13 12:09:23 -0700

    Update Layout.jsx

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,1 +1,15 @@
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
+export default function Layout() {
+  return (
+    <>
+      <Header />
+      <main style={{ padding: "20px" }}>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
